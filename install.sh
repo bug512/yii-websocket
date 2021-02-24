4ef8b89b38426c12b0a1b3182ea6571898da14dc
Max Mezhennikov <mezhennikov@mail.tvil.ru>
2021-02-24 14:34:00 +0300

    Hotfix install scrypts

--- install.sh
+++ install.sh
@@ -4,13 +4,33 @@ if [ ! -f .env ]; then
     cp .env.example .env
 fi
 
+if [ ! -d web/admin/runtime ]; then
+    mkdir web/admin/runtime
+fi
+
 sudo chmod -R 777 web/admin/runtime
+
+if [ ! -d web/frontend/runtime ]; then
+    mkdir web/frontend/runtime
+fi
 
 sudo chmod -R 777 web/frontend/runtime
 
+if [ ! -d web/frontend/www/assets ]; then
+    mkdir web/frontend/www/assets
+fi
+
 sudo chmod -R 777 web/frontend/www/assets
 
+if [ ! -d web/admin/www/assets ]; then
+    mkdir web/admin/www/assets
+fi
+
 sudo chmod -R 777 web/admin/www/assets
+
+npm install --prefix web/admin/www/
+
+npm install --prefix web/frontend/www/
 
 docker-compose up -d
 
